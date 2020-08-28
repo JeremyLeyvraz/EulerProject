a656906726df2f43fb3bcf28e03edc92b8e52f56
Jérémy Leyvraz <32959624+JeremyLeyvraz@users.noreply.github.com>
2020-08-28 15:31:08 +0200

    Add visual studio project (#1)

---
+++ C#/EulerProject/Solutions.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Solutions.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Solutions.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7257cb08-0159-42e5-a2fe-cfeb1aa8403c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 This repository contains the solutions to the Euler project problems.
 
 The solutions are coded with C#, and Python.
+
+Site web:
+https://projecteuler.net/about
