da9eba804a22900265bef5a34104968b1faaeba4
Jérémy Leyvraz <32959624+JeremyLeyvraz@users.noreply.github.com>
2020-08-29 17:32:50 +0200

    Add solution of problem 0006 (#7)

---
+++ C#/EulerProject/Solutions.UnitTests/Problem_0006.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.UnitTests
+{
+    /// <summary>
+    /// Solution of Problem_0006
+    /// </summary>
+    [TestClass]
+    public class Problem_0006
+    {
+        [TestMethod]
+        public void Solution_0006()
+        {
+            // Act
+            var result = Solutions.Problem_0006.Solve();
+
+            // Assert
+            Assert.AreEqual(25164150, result);
+        }
+    }
+}
---
+++ C#/EulerProject/Solutions/Solutions/Problem_0006.cs
@@ -0,0 +1,32 @@
+﻿namespace Solutions.Solutions
+{
+    /// <summary>
+    /// The sum of the squares of the first ten natural numbers is:
+    /// 1² + 2² + 3² + ... + 9² + 10² = 385
+    /// 
+    /// The square of the sum of the first ten natural numbers is:
+    /// (1 + 2 + 3 + ... + 10)² = 3025
+    /// 
+    /// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is:
+    /// 3025 - 385 = 2640
+    /// 
+    /// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    /// </summary>
+    public static class Problem_0006
+    {
+        public static int Solve()
+        {
+            var x = 100;
+            var sumOfSquares = 0;
+            var sumOfNumber = 0;
+
+            for (int i = 1; i <= x; i++)
+            {
+                sumOfNumber += i;
+                sumOfSquares += i * i;
+            }
+
+            return sumOfNumber * sumOfNumber - sumOfSquares;
+        }
+    }
+}
