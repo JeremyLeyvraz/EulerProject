c9e2349fea7138552fd7863a49b46006ae6491b3
Jérémy Leyvraz <32959624+JeremyLeyvraz@users.noreply.github.com>
2020-08-29 15:02:26 +0200

    Add solution to the problem 0003 (#3)

---
+++ C#/EulerProject/Solutions.UnitTests/Problem_0003.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.UnitTests
+{
+    /// <summary>
+    /// Solution for Problem_0003
+    /// </summary>
+    [TestClass]
+    public class Problem_0003
+    {
+        [TestMethod]
+        public void Solution_0003()
+        {
+            // Act
+            var result = Solutions.Problem_0003.Solve(600851475143);
+
+            // Assert
+            Assert.AreEqual(6857, result);
+        }
+    }
+}
---
+++ C#/EulerProject/Solutions/Solutions/Problem_0003.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Solutions
+{
+    /// <summary>
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// 
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
+    public static class Problem_0003
+    {
+        public static int Solve(long argument)
+        {
+            var primeFactor = new List<int>();
+
+            var middle = argument / 2.0;
+            var currentQuotient = argument;
+
+            for (int i = 2; i < middle; i++)
+            {
+                if (!IsPrime(i))
+                    continue;
+
+                while (currentQuotient > 1 && (currentQuotient / (double)i) % 1 == 0)
+                {
+                    primeFactor.Add(i);
+                    currentQuotient = currentQuotient / i;
+                }
+
+                if (currentQuotient <= 1)
+                    break;
+            }
+
+            return primeFactor.LastOrDefault();
+        }
+
+        /// <summary>
+        /// Determine if x is prime
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>True if x is prime, false otherwise</returns>
+        private static bool IsPrime(int x)
+        {
+            if (x < 2)
+                return false;
+
+            double middle = x / 2.0;
+
+            for (int i = 2; i < middle; i++)
+            {
+                var quotient = x / (double)i;
+                if (quotient % 1 == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
