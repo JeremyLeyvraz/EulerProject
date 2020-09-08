3554498397e38f0bb59f73d91600a0a4590a7604
Jérémy Leyvraz <32959624+JeremyLeyvraz@users.noreply.github.com>
2020-09-08 14:00:19 +0200

    Add solution of problem 0011
    * Add problem template
    
    * Add convert to array method
    
    * Add solution for problem 11
    
    Co-authored-by: JeremyLeyvraz <jeremy.leyvraz@econocom.com>

---
+++ C#/EulerProject/Solutions.UnitTests/Problem_0011.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.UnitTests
+{
+    /// <summary>
+    /// Solution of Problem_0011
+    /// </summary>
+    [TestClass]
+    public class Problem_0011
+    {
+        [TestMethod]
+        public void Solution_0011()
+        {
+            // Act
+            var result = Solutions.Problem_0011.Solve();
+
+            // Assert
+            Assert.AreEqual(70600674, result);
+        }
+    }
+}
